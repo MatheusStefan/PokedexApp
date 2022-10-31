78db412b5413712509730d10a2cadb2645927c6d
MatheusStefan <roccomatheus0@gmail.com>
2022-10-31 18:18:19 -0300

    Modelo personalizado de pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,16 +3,36 @@
 
 const pokeAPI = {}
 
-pokeAPI.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeAPI.getPokemons = (offset = 0, limit = 5) => {
+pokeAPI.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
+}
+
+pokeAPI.getPokemons = (offset = 0, limit = 15) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    //busca de lista de pokemons
     return fetch(url)
-        .then((response) => response.json())
-        .then(jsonBody => jsonBody.results)
-        .then((pokemons) => pokemons.map((pokeAPI.getPokemonDetail)))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then((response) => response.json()) //conversao para json
+        .then(jsonBody => jsonBody.results) //lista de pokemons convertida
+        .then((pokemons) => pokemons.map((pokeAPI.getPokemonDetail))) //conversao para pokemons detalhados
+        .then((detailRequests) => Promise.all(detailRequests)) //chamada com varias requisicoes + conversao para json
+        .then((pokemonsDetails) => pokemonsDetails) //lista detalhada de pokemons, modelo PokeAPI
 }
