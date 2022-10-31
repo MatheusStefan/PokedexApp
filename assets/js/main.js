0ec965a85275a93c9ad4fa3730a061e2ee4821a3
MatheusStefan <roccomatheus0@gmail.com>
2022-10-31 10:40:50 -0300

    Separação de API e HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToHtml(pokemon) {
     return `
@@ -23,15 +21,13 @@ function convertPokemonToHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
- fetch(url)
-     .then((response) => response.json())
-     .then(jsonBody => jsonBody.results)
-     .then((pokemons) => {
+pokeAPI.getPokemons().then((pokemons) => {
 
-         for (let i = 0; i < pokemons.length; i++) {
-             const pokemon = pokemons[i];
-             pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-            }
-        
-     })
-     .catch((error) => console.error(error))
+    const listItems = []
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToHtml(pokemon))
+    }
+    
+    console.log(listItems)
+})
